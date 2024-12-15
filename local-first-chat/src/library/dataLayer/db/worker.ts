be72478a7650b6545e6ec676495bc3c87a50e863
Alex Nahas <alexmnahas@gmail.com>
2024-12-15 10:02:45 -0500

    clean up

--- local-first-chat/src/library/dataLayer/db/worker.ts
+++ local-first-chat/src/library/dataLayer/db/worker.ts
@@ -1,18 +1,20 @@
 import { PGlite } from '@electric-sql/pglite';
 import { worker } from '@electric-sql/pglite/worker';
 import { drizzle } from 'drizzle-orm/pglite/driver';
-import { migrate } from 'drizzle-orm/pglite/migrator';
 import type { MigrationConfig } from 'drizzle-orm/migrator';
 import migrations from './migrations.json';
+import { syncShapeToTable } from '@makisuo/pglite-drizzle';
+import { OpfsAhpFS } from '@electric-sql/pglite/opfs-ahp';
 
 worker({
   async init() {
     const pg = new PGlite({
-      dataDir: 'idb://chat_database',
+      fs: new OpfsAhpFS('chatDB'),
       relaxedDurability: true,
     });
     const db = drizzle(pg);
-    //@ts-ignore
+
+    //@ts-expect-error internal drizzle type issue
     db.dialect.migrate(migrations, db.session, {
       migrationsTable: 'drizzle_migrations',
     } satisfies Omit<MigrationConfig, 'migrationsFolder'>);
